7cc68842e36901ebecd049356be0b53b2a13166c
YulianaKoprinkova <yuliana.koprinkova@gmail.com>
2025-05-13 22:24:46 +0300

    all errors cleared

--- src/test/java/org/yulianakoprinkova/NewLogin.java
+++ src/test/java/org/yulianakoprinkova/NewLogin.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class NewLogin {
 
     @Test
@@ -16,8 +18,13 @@ public class NewLogin {
 
         driver.get (LOGIN_PAGE_URL);
 
+        WebDriver.Timeouts timeouts = driver.manage().timeouts().pageLoadTimeout(Duration.ofMillis(2000));
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         //UI MAP
         //SIGNUP FORM
+        //
 
         //1.2. USERNAME
         String NEW_USERNAME_INPUT_FIELD_CSS = "input.form-control[formcontrolname=\"username\"]";
@@ -43,75 +50,42 @@ public class NewLogin {
         WebElement newUserName = driver.findElement(By.cssSelector(NEW_USERNAME_INPUT_FIELD_CSS));
 
         newUserName.click();
-        Thread.sleep(1000);
-
-        //newUserName.clear();
-        //Thread.sleep(1000);
-
+        newUserName.clear();
         newUserName.sendKeys("Name Name");
-        Thread.sleep(1000);
+
 
         WebElement email = driver.findElement(By.cssSelector(EMAIL_INPUT_FIELD_CSS));
 
         email.click();
-        Thread.sleep(1000);
-
-        //email.clear();
-        //Thread.sleep(1000);
-
+        email.clear();
         email.sendKeys("myemail@provider.com");
-        Thread.sleep(1000);
+
 
         WebElement birthDate = driver.findElement(By.cssSelector(DOB_INPUT_FIELD_CSS));
-
         birthDate.click();
-        Thread.sleep(1000);
-
-        //birthDate.clear();
-        //Thread.sleep(1000);
-
+        birthDate.clear();
         birthDate.sendKeys("07/07/2011");
-        Thread.sleep(1000);
+
 
         WebElement password = driver.findElement(By.cssSelector(PASSWORD_INPUT_FIELD_CSS));
         password.click();
-        Thread.sleep(1000);
-
-        //password.clear();
-        //Thread.sleep(1000);
-
+        password.clear();
         password.sendKeys("MyPass#123");
-        Thread.sleep(1000);
 
         WebElement confirmPassword = driver.findElement(By.cssSelector(CONFIRM_PASSWORD_INPUT_FIELD_CSS));
-
         confirmPassword.click();
-        Thread.sleep(1000);
-
-        //confirmPassword.clear();
-        //Thread.sleep(1000);
-
+        confirmPassword.clear();
         confirmPassword.sendKeys("MyPass#123");
-        Thread.sleep(1000);
 
         WebElement publicInfo = driver.findElement(By.cssSelector(PUBLIC_INFO_INPUT_FIELD_CSS));
-
         publicInfo.click();
-        Thread.sleep(1000);
-
-        //publicInfo.clear();
-        //Thread.sleep(1000);
-
-        publicInfo.sendKeys("MyPass#123");
-        Thread.sleep(1000);
+        publicInfo.clear();
+        publicInfo.sendKeys("?Public Info?");
 
         WebElement signInButton;
         signInButton = driver.findElement(By.cssSelector(SIGN_IN_BUTTON_CSS));
-
         signInButton.click();
-        Thread.sleep(1000);
 
-        Thread.sleep(13000);
 
         driver.quit();
 
