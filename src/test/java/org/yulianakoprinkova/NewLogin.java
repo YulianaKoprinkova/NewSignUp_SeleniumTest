96ededecffe0534b470affc6ae5ee5e1ae4e578c
YulianaKoprinkova <yuliana.koprinkova@gmail.com>
2025-05-14 18:48:57 +0300

    final -1 version

--- src/test/java/org/yulianakoprinkova/NewLogin.java
+++ src/test/java/org/yulianakoprinkova/NewLogin.java
@@ -4,114 +4,133 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-import java.lang.Math;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+import static java.awt.SystemColor.text;
+import static java.lang.Thread.sleep;
+
 
 public class NewLogin {
 
-    //UI MAP
-    //SIGNUP FORM
-    //
+    //1. URLs
+    private static final String REGISTER_PAGE_URL = "http://training.skillo-bg.com:4300/users/register";
+    //private static final String LOGIN_PAGE_URL = "http://training.skillo-bg.com:4300/users/login";
 
-    //1.2. USERNAME
-    String NEW_USERNAME_INPUT_FIELD_CSS = "input.form-control[formcontrolname=\"username\"]";
+    private static final String EXPECTED_REG_FORM_HEADER_TEXT = "Sign up";
 
-    //1.3. EMAIL
-    String EMAIL_INPUT_FIELD_CSS = "input.form-control[formcontrolname=\"email\"]";
+    String SIGN_UP_FORM_HEADER_XPATH = "//h4";
 
-    //1.3. DOB
-    String DOB_INPUT_FIELD_CSS = "input.form-control[formcontrolname=\"birthDate\"]";
+    //2. Locators
+    //2.1 Registration form attributes:
+    String NEW_USER_USERNAME_XPATH = "//input[contains(@name,\"username\")]";
+    String NEW_USER_EMAIL_XPATH = "//input[contains(@type, \"email\")]";
+    String NEW_USER_BIRTHDATE_XPATH = "//input[contains(@formcontrolname, \"birthDate\")]";
+    String NEW_USER_PASSWORD_CSS = "#defaultRegisterFormPassword";
+    String NEW_USER_CONFIRM_PASSWORD_CSS = "#defaultRegisterPhonePassword";
+    String NEW_USER_PUBLIC_INFO_XPATH = "//textarea";
+    String NEW_USER_SIGN_IN_BUTTON_CSS = "#sign-in-button";
 
-    //1.4. PASSWORD
-    String PASSWORD_INPUT_FIELD_CSS = "input.form-control[formcontrolname=\"password\"]";
+    //3. When logged in
+    // Reg page error messages
+    // Reg page layouts
+    String PROFILE_BTN_CSS = "#nav-link-profile";
 
-    //1.5 CONFIRM PASSWOR
-    String CONFIRM_PASSWORD_INPUT_FIELD_CSS = "input.form-control[formcontrolname=\"confirmPassword\"]";
+    //4. Driver
+    public WebDriver driver = new ChromeDriver();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
-    //1.6 PUBLIC INFO
-    String PUBLIC_INFO_INPUT_FIELD_CSS = "textarea.form-control[formcontrolname=\"publicInfo\"]";
-
-    //1.7. SIGN IN BUTTON
-    String SIGN_IN_BUTTON_CSS = "#sign-in-button";
-
-
-    private WebDriver driver;
-
-
-    @BeforeTest
-    public void setUpDriver () {
-        driver = new ChromeDriver();
+    @BeforeMethod
+    public void initiateBrowser() {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
 
-    @BeforeTest
-    public void beforeSeleniumTest () {
-        System.out.println("Testing Registration of New User");
+    @AfterMethod
+    public void quitDriver() throws InterruptedException {
+        Thread.sleep(10000);
+
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
-    public void myNewSkilloUserSeleniumTest() throws InterruptedException {
+    public void newUserRegistrationTest() throws InterruptedException {
 
-        String LOGIN_PAGE_URL = "http://training.skillo-bg.com:4300/users/register";
+        driver.get(REGISTER_PAGE_URL);
+        String actualLoadedUrl = driver.getCurrentUrl();
+        Assert.assertEquals(REGISTER_PAGE_URL, actualLoadedUrl);
 
-        driver.get (LOGIN_PAGE_URL);
+        WebElement regFormHeader = driver.findElement(By.xpath(SIGN_UP_FORM_HEADER_XPATH));
+        String actualregFormHeaderText = regFormHeader.getText();
+        Assert.assertEquals(EXPECTED_REG_FORM_HEADER_TEXT, actualregFormHeaderText);
 
-        driver.manage().window().maximize();
+        WebElement newUserNameField = driver.findElement(By.xpath(NEW_USER_USERNAME_XPATH));
+        String provideUserName = demoUserName();
+        formFillOut(newUserNameField, provideUserName);
 
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofMillis(2000));
+        WebElement newUserEmailField = driver.findElement(By.xpath(NEW_USER_EMAIL_XPATH));
+        String provideEmail = demoEmail();
+        formFillOut(newUserEmailField, provideEmail);
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement newUserBirthdateField = driver.findElement(By.xpath(NEW_USER_BIRTHDATE_XPATH));
+        formFillOut(newUserBirthdateField, "07072001");
+
+        String password;
+        Random random = new Random();
+        password = ("A#a" + random.nextInt(999));
+
+        WebElement newUserPasswordField = driver.findElement(By.cssSelector(NEW_USER_PASSWORD_CSS));
+        formFillOut(newUserPasswordField, password);
+
+        WebElement newUserRepeatPasswordField = driver.findElement(By.cssSelector(NEW_USER_CONFIRM_PASSWORD_CSS));
+        formFillOut(newUserRepeatPasswordField, password);
+
+        WebElement newUserPublicInfoField = driver.findElement(By.xpath(NEW_USER_PUBLIC_INFO_XPATH));
+        String providePublicInfo = "AAaaBBbbCCcc";
+        formFillOut(newUserPublicInfoField, providePublicInfo);
+
+        WebElement newUserSigInButton = driver.findElement(By.cssSelector(NEW_USER_SIGN_IN_BUTTON_CSS));
+        newUserSigInButton.click();
 
 
-
-        WebElement newUserName = driver.findElement(By.cssSelector(NEW_USERNAME_INPUT_FIELD_CSS));
-
-        newUserName.click();
-        newUserName.clear();
-        newUserName.sendKeys("Name Name");
-
-        WebElement email = driver.findElement(By.cssSelector(EMAIL_INPUT_FIELD_CSS));
-
-        email.click();
-        email.clear();
-        email.sendKeys("myemail@provider.com");
-
-        WebElement birthDate = driver.findElement(By.cssSelector(DOB_INPUT_FIELD_CSS));
-        birthDate.click();
-        birthDate.clear();
-        birthDate.sendKeys("07/07/2011");
-
-        WebElement password = driver.findElement(By.cssSelector(PASSWORD_INPUT_FIELD_CSS));
-        password.click();
-        password.clear();
-
-        double rand = Math.random();
-        password.sendKeys("A" + rand);
-
-        WebElement confirmPassword = driver.findElement(By.cssSelector(CONFIRM_PASSWORD_INPUT_FIELD_CSS));
-        confirmPassword.click();
-        confirmPassword.clear();
-        confirmPassword.sendKeys("A" + rand);
-
-        WebElement publicInfo = driver.findElement(By.cssSelector(PUBLIC_INFO_INPUT_FIELD_CSS));
-        publicInfo.click();
-        publicInfo.clear();
-        publicInfo.sendKeys("?Public Info?");
-
-        WebElement signInButton;
-        signInButton = driver.findElement(By.cssSelector(SIGN_IN_BUTTON_CSS));
-        signInButton.click();
-
-        driver.quit();
 
     }
 
-    @AfterTest
-    public void afterSeleniumTest () {
-        System.out.println("New Registration Test Was Executed");
+        public String getCurrentTime() {
+            LocalDateTime time = LocalDateTime.now();
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("hhmmss");
+            String formattedDateTime = time.format(format);
+            return formattedDateTime;
+        }
+
+        public String demoUserName () {
+            String username = "User" + getCurrentTime();
+            return username;
+        }
+
+        public String demoEmail () {
+            String email = "Pass" + getCurrentTime() + "@mail.com";
+            return email;
+        }
+
+        public void formFillOut(WebElement element, String text) {
+        element.clear();
+        element.sendKeys(text);
+        }
+
+
+
     }
 
-}
+
+
